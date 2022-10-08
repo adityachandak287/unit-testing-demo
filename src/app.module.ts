1d72a72faaf8bfb04bc3a37524215ed5b51028f9
Aditya Chandak <adityachandak287@gmail.com>
2022-10-08 20:06:13 +0530

    feat: add prisma for db access and create user repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaClient, Prisma } from '@prisma/client';
+import { join } from 'path';
+import { UserRepository } from './repositories/db/user.repository';
+
+const DB_OPTIONS: Prisma.PrismaClientOptions = {
+  datasources: {
+    db: {
+      url: 'file:' + join(process.cwd(), 'sqlite.db'),
+    },
+  },
+};
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: 'DATABASE_CLIENT',
+      useValue: new PrismaClient(DB_OPTIONS),
+    },
+    UserRepository,
+    AppService,
+  ],
 })
 export class AppModule {}
