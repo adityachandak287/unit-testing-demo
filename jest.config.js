09db7398f8f1cfa3ee7072bb17d2fb95a8c6d9dd
Aditya Chandak <adityachandak287@gmail.com>
2022-10-08 18:19:09 +0530

    docs: update readme with commands

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,18 @@ module.exports = {
   },
   testEnvironment: 'node',
   collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverage: true,
+  verbose: true,
   coverageDirectory: '../coverage',
   coverageReporters: ['lcov', 'text', 'text-summary'],
   bail: true,
   detectOpenHandles: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
