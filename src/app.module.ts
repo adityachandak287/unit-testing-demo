df88983c4d4b9dd2f9534da4e38a260a08210df2
Aditya Chandak <adityachandak287@gmail.com>
2022-10-09 00:57:55 +0530

    feat: delete redundant UserRepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaClient, Prisma } from '@prisma/client';
 import { join } from 'path';
-import { UserRepository } from './repositories/db/user.repository';
+
 
 const DB_OPTIONS: Prisma.PrismaClientOptions = {
   datasources: {
@@ -21,7 +21,6 @@ const DB_OPTIONS: Prisma.PrismaClientOptions = {
       provide: 'DATABASE_CLIENT',
       useValue: new PrismaClient(DB_OPTIONS),
     },
-    UserRepository,
     AppService,
   ],
 })
